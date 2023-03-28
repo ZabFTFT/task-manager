c8740053aa1582fe31347145f6f0003b2798c2d7
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-03-28 20:43:18 +0300

    add todo_list view, html file and url

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from tasks.views import index
+from tasks.views import index, ToDoList
 
 app_name = "tasks"
 
 urlpatterns = [
     path("", index, name="index"),
+    path("to_do_list/", ToDoList.as_view(), name="ToDoList"),
 
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,14 @@
+from django.db.models import Q
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
 from tasks.models import Task
 
 
 def index(request: HttpRequest) -> HttpResponse:
     num_task_all = Task.objects.count()
-    num_task_in_progress = Task.objects.filter(in_progress=True)
+    num_task_in_progress = Task.objects.filter(in_progress=True).count()
 
     context = {
         "num_task_all": num_task_all,
@@ -14,3 +16,12 @@ def index(request: HttpRequest) -> HttpResponse:
     }
 
     return render(request, "tasks/index.html", context=context)
+
+
+class ToDoList(generic.ListView):
+    model = Task
+    queryset = Task.objects.filter(is_completed=False, in_progress=False)
+    template_name = "tasks/todo_list.html"
+    context_object_name = "todo_list"
+
+

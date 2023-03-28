de4ea339b011d700c0356851c1d18991ebd35a39
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-03-28 19:15:08 +0300

    update project urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -20,6 +20,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("tasks.urls", namespace="tasks")),
+    path("accounts/", include("django.contrib.auth.urls")),
 
 
     path('__debug__/', include('debug_toolbar.urls')),

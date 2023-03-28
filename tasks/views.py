d7c7e04efe261739bcba118926e6521bc5b9b073
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-03-28 19:28:32 +0300

    add urls.py to tasks, index view

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from tasks.models import Task
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_task_all = Task.objects.count()
+    num_task_in_progress = Task.objects.filter(in_progress=True)
+
+    context = {
+        "num_task_all": num_task_all,
+        "num_task_in_progress": num_task_in_progress,
+    }
+
+    return render(request, "tasks/index.html", context=context)

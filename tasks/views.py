03e6063bcf792d2d56f69df9aba288cdda2e3097
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-03-30 15:08:47 +0300

    refactor flake8 and few templates corrections

--- tasks/views.py
+++ tasks/views.py
@@ -3,7 +3,6 @@ import datetime
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db.models import Count
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
@@ -12,6 +11,7 @@ from django.views import generic
 from tasks.filters import OrderFilter
 from tasks.forms import CreateTaskForm, CreateWorkerForm
 from tasks.models import Task
+
 
 @login_required
 def index(request: HttpRequest) -> HttpResponse:
@@ -35,7 +35,10 @@ class ProjectTaskListView(LoginRequiredMixin, generic.ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         tasks_not_completed = Task.objects.filter(is_completed=False)
-        project_tasks_filter = OrderFilter(self.request.GET, queryset=tasks_not_completed)
+        project_tasks_filter = OrderFilter(
+            self.request.GET,
+            queryset=tasks_not_completed
+        )
         project_tasks_filter_qs = project_tasks_filter.qs
         context["filter"] = project_tasks_filter
         context["tasks"] = tasks_not_completed
@@ -50,10 +53,13 @@ class ProjectTaskListFinishedView(LoginRequiredMixin, generic.ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         finished_tasks = Task.objects.filter(is_completed=True)
-        participants = get_user_model().objects.filter(tasks__in=finished_tasks).distinct()
+        participants = get_user_model().objects.filter(
+            tasks__in=finished_tasks
+        ).distinct()
         context["finished_tasks"] = finished_tasks
         context["participants"] = participants
         return context
+
 
 class CreateTaskView(LoginRequiredMixin, generic.CreateView):
     form_class = CreateTaskForm
@@ -75,7 +81,9 @@ class ProjectTaskDetailView(LoginRequiredMixin, generic.DetailView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         finished_task = Task.objects.get(pk=self.kwargs["pk"])
-        participants = get_user_model().objects.filter(tasks__in=[finished_task]).distinct()
+        participants = get_user_model().objects.filter(
+            tasks__in=[finished_task]
+        ).distinct()
         context["finished_tasks"] = finished_task
         context["participants"] = participants
         return context
@@ -87,10 +95,18 @@ class CreateWorkerView(LoginRequiredMixin, generic.CreateView):
     success_url = reverse_lazy("login")
 
 
+@login_required
 def personal_task_list(request, pk):
-    to_do_tasks = Task.objects.filter(in_progress=False, assignees__in=[pk], is_completed=False)
-    in_progress_tasks = Task.objects.filter(in_progress=True, assignees__in=[pk], is_completed=False)
-    completed_tasks = Task.objects.filter(is_completed=True, assignees__in=[pk])
+    to_do_tasks = Task.objects.filter(
+        in_progress=False,
+        assignees__in=[pk],
+        is_completed=False
+    )
+    in_progress_tasks = Task.objects.filter(
+        in_progress=True,
+        assignees__in=[pk],
+        is_completed=False
+    )
 
     if request.method == 'POST':
         if request.POST.get("task_id_done"):
@@ -99,17 +115,17 @@ def personal_task_list(request, pk):
             task.is_completed = True
             task.in_progress = False
             task.completed_time = datetime.datetime.now()
+            task.save()
         elif request.POST.get("task_id"):
             task_id = request.POST.get('task_id')
             task = Task.objects.get(id=task_id)
             task.in_progress = True
-        task.save()
+            task.save()
         return HttpResponseRedirect(request.path_info)
 
     context = {
         'to_do_tasks': to_do_tasks,
         'in_progress_tasks': in_progress_tasks,
-        "completed_tasks": completed_tasks,
     }
 
     return render(request, 'tasks/my_tasks_page.html', context)

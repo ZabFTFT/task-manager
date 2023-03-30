21fe14805e38270909501a0d3f2a0dbd01f198e0
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-03-30 09:29:32 +0300

    refactor namespaces and links in view and templates

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,8 @@
 import datetime
 
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
@@ -10,7 +12,7 @@ from tasks.filters import OrderFilter
 from tasks.forms import CreateTaskForm, CreateWorkerForm
 from tasks.models import Task
 
-
+@login_required
 def index(request: HttpRequest) -> HttpResponse:
     num_task_all = Task.objects.count()
     num_task_in_progress = Task.objects.filter(in_progress=True).count()
@@ -23,7 +25,7 @@ def index(request: HttpRequest) -> HttpResponse:
     return render(request, "tasks/index.html", context=context)
 
 
-class ProjectTaskListView(generic.ListView):
+class ProjectTaskListView(LoginRequiredMixin, generic.ListView):
     model = Task
     template_name = "tasks/project_task_all.html"
 
@@ -38,7 +40,7 @@ class ProjectTaskListView(generic.ListView):
         return context
 
 
-class ProjectTaskListFinishedView(generic.ListView):
+class ProjectTaskListFinishedView(LoginRequiredMixin, generic.ListView):
     model = Task
     template_name = "tasks/project_task_finished.html"
 
@@ -49,25 +51,34 @@ class ProjectTaskListFinishedView(generic.ListView):
         context["finished_tasks"] = finished_tasks
         context["participants"] = participants
         return context
-class CreateTaskView(generic.CreateView):
+
+class CreateTaskView(LoginRequiredMixin, generic.CreateView):
     form_class = CreateTaskForm
     template_name = "tasks/create_project_task.html"
     success_url = reverse_lazy("tasks:project-tasks-list")
 
 
-class DeleteTaskView(generic.DeleteView):
+class DeleteTaskView(LoginRequiredMixin, generic.DeleteView):
     model = Task
     template_name = "tasks/project_task_delete_confirm.html"
     success_url = reverse_lazy("tasks:project-tasks-list")
 
 
-class ProjectTaskDetailView(generic.DetailView):
+class ProjectTaskDetailView(LoginRequiredMixin, generic.DetailView):
     model = Task
     template_name = "tasks/project_task_detail.html"
     context_object_name = "project_task"
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        finished_task = Task.objects.get(pk=self.kwargs["pk"])
+        participants = get_user_model().objects.filter(tasks__in=[finished_task]).distinct()
+        context["finished_tasks"] = finished_task
+        context["participants"] = participants
+        return context
 
-class CreateWorkerView(generic.CreateView):
+
+class CreateWorkerView(LoginRequiredMixin, generic.CreateView):
     form_class = CreateWorkerForm
     template_name = "tasks/create_worker.html"
     success_url = reverse_lazy("tasks:worker-create")
@@ -98,4 +109,4 @@ def personal_task_list(request, pk):
         "completed_tasks": completed_tasks,
     }
 
-    return render(request, 'tasks/profile_page.html', context)
+    return render(request, 'tasks/my_tasks_page.html', context)
